9dbafee5ee4ebe58191e85c655f702a6e5c3d101
snozaki <snozaki@netprotections.co.jp>
2023-03-24 21:11:44 +0900

    refactor 依存ライブラリのバージョンを引き上げる

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
-    entry: "./js/main.js",
-    output: {
-        filename: "bundle.js",
-        path: path.join(__dirname, "public")
-    }
+  mode: "production",
+  entry: "./js/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  }
 }
